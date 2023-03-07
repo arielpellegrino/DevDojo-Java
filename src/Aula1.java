b967c1f714b48522ebc5ad54eb937912619f0d59
Ariel Pellegrino Iborra <ariel.iborra@sensedia.com>
2023-03-07 20:35:38 -0300

    arquivos de aula dia 7/3

---
+++ src/Aula1.java
@@ -0,0 +1,8 @@
+package src;
+
+public class Aula1 {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+
+}
---
+++ src/Aula2tiposPrimitivos.java
@@ -0,0 +1,10 @@
+package src;
+
+public class Aula2tiposPrimitivos {
+    public static void main(String[] args) {
+        //int, double, float, char, byte, short, long boolean
+        int age = 10;
+        System.out.println(age);
+
+    }
+}
